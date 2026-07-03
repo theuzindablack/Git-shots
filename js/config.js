db18f2c9b9da5126a9e3a1e1f6d4f9914bddc654
theuzindablack <mgoulartdasilva2@gmail.com>
2026-07-03 15:31:37 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,3 @@
+export const GITHUB_CLIENT_ID = 'Ov23liFmRRxAl4bPsMqa';
+// ↓ COLOQUE AQUI A URL DO SEU SITE NO GITHUB PAGES
+export const REDIRECT_URI = 'https://theuzindablack.github.io/Git-shots/';
